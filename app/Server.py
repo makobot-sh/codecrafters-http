07f068c363f6260d9d41a44ea128dca67cb919f4
makobot-sh <45239258+makobot-sh@users.noreply.github.com>
2025-01-27 22:12:46 -0300

    Simple Response and Request parsing working

--- app/Server.py
+++ app/Server.py
@@ -1,11 +1,22 @@
 import socket  # noqa: F401
 
-class Response:
+class Message():
+    def print(self):
+        res = self.render().decode()
+        if len(self._headers.keys()) == 0:
+            res = res[:-2]
+            if self.body == "": #If there's not headers or body remove the carriage return separating them from request line as well
+                res = res[:-2]
+        if self.body == "":
+            res = res[:-2]
+        return res
+
+class Response(Message):
     def __init__(self, http_version=1.1, status_code=None, status_reason=""):
         self.http_version = http_version
         self.status_code = status_code
         self.status_reason = status_reason
-        self.headers = {}
+        self._headers = {}
         self.body = ""
 
     def render(self):
@@ -13,16 +24,16 @@ class Response:
             print("Status code must be specified before rendering!")
             raise Exception("Tried to render a response without a status code set")
         status = f"HTTP/{self.http_version} {self.status_code} {self.status_reason}"
-        headers = "\r\n".join(self.headers)
+        headers = "\r\n".join(self._headers)
         response = f"{status}\r\n{headers}\r\n{self.body}\r\n"
         return str.encode(response)
 
-class Request:
+class Request(Message):
     def __init__(self, http_version=1.1, method="GET", target=None):
         self.http_version = http_version
         self.method = method
         self.target = target
-        self.headers = {}
+        self._headers = {}
         self.body = ""
 
     def addHeader(self, key, value):
@@ -30,40 +41,57 @@ class Request:
 
     @property
     def request_line(self):
-        return f"{self.method} {self.target} HTTP/{self.http_version}\r\n"
+        return f"{self.method} {self.target} HTTP/{self.http_version}"
     
     @request_line.setter
     def request_line(self, req_str):
         self.method, self.target, http_version = req_str.split(" ")
         assert http_version.startswith("HTTP/"), "HTTP version should start with HTTP/"
         self.http_version = http_version[5:]
+
+    @property
+    def headers(self):
+        return "\r\n".join([f"{k}: {v}" for k,v in self._headers.items()])
+    
+    @headers.setter
+    def headers(self, str_headers: str | list):
+        if isinstance(str_headers, str):
+            header_list = str_headers.split("\r\n")
+        else:
+            header_list = str_headers
+        for str_header in header_list:
+            if str_header == "":
+                continue
+            kv = str_header.split(":", 1)
+            self._headers[kv[0]] = kv[1]
     
     def parse(msg):
         dec_msg = msg.decode()
-        print(dec_msg)
         parts = dec_msg.split("\r\n")
 
         req = Request()
         req.request_line = parts[0]
+        req.headers = parts[1:-1]
+        req.body = parts[-1]
 
+        return req
 
     def render(self):
-        if self.status_code is None:
-            print("Status code must be specified before rendering!")
-            raise Exception("Tried to render a response without a status code set")
-        status = f"HTTP/{self.http_version} {self.status_code} {self.status_reason}"
-        headers = "\r\n".join(self.headers)
-        response = f"{status}\r\n{headers}\r\n{self.body}\r\n"
+        if self.target is None:
+            print("Target must be specified before rendering!")
+            raise Exception("Tried to render a request without a target set")
+        response = f"{self.request_line}\r\n{self.headers}\r\n{self.body}\r\n"
         return str.encode(response)
 
 class Server:
     def __init__(self, address: tuple[str,str]=("localhost", 4221), **kwargs):
         self.server_socket = socket.create_server(address, **kwargs)
+        self.srv_host, self.srv_addr = address
     
     def await_incoming_connection(self):
         self.conn, self.addr = self.server_socket.accept() # wait for client
         
-        print(f"Connected by {self.addr}")
+        print(f"[{self.srv_host}:{self.srv_addr}] Connected by {self.addr}")
         self.OK_200()
 
     def listen(self):
@@ -71,17 +99,20 @@ class Server:
             while True:
                 data = self.conn.recv(1024)
                 if not data: break
-                print(f"[{self.addr[0]}:{self.addr[1]}] {data.decode()}")
+                req = Request.parse(data)
+                print(f"[{self.addr[0]}:{self.addr[1]}] {req.print()}")
+                if req.target == "/":
+                    self.OK_200()
+                else:
+                    self.NOT_FOUND_404()
 
-                Request.parse(data)
-
-                self.OK_200()
+    def send(self, res: Response):
+        print(f"[{self.srv_host}:{self.srv_addr}] {res.print()}")
+        self.conn.sendall(res.render())
 
     def OK_200(self):
-        res = Response(status_code=200, status_reason="OK")
-        self.conn.sendall(res.render())
+        self.send(Response(status_code=200, status_reason="OK"))
     
     def NOT_FOUND_404(self):
-        res = Response(status_code=404, status_reason="Not Found")
-        self.conn.sendall(res.render())
+        self.send(Response(status_code=404, status_reason="Not Found"))
     
--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from app.Server import Server
 
 def main():
     server = Server(reuse_port=True)
-    server.await_incoming_connection()
-    server.listen()
+    while True:
+        server.await_incoming_connection()
+        server.listen()
 
 if __name__ == "__main__":
     main()
